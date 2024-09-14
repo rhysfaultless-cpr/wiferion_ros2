901a9eff9dbb0963a595ac886776ae0caa7ba659
Rhys Faultless <rfaultless@clearpathrobotics.com>
2024-09-14 07:43:15 -0400

    Added current readings. I have not tested this while charging a battery, as I am currently using a lab power supply to simplify development. This program is publishing 0.3 A, but this may just be the Wiferion default reported value through CAN. The lab supply is showing 0.0 A consumed.

--- wiferion_ros2/wiferion_ros2/wiferion_ros2.py
+++ wiferion_ros2/wiferion_ros2/wiferion_ros2.py
@@ -49,24 +49,52 @@ class WiferionCharger(Node):
 
         self.charger_publisher_voltage = None
         self.charger_publisher_voltage = self.create_publisher(Float32, 'wiferion_charger_voltage', 10)
+        self.charger_publisher_current = None
+        self.charger_publisher_current = self.create_publisher(Float32, 'wiferion_charger_current', 10)
         
 
     def timer_callback(self):
         try:
             while True:
+                # while loop, to wait for a CAN message that meets the message ID '18ff50e5'
                 received_message = self.can_bus.recv()
                 received_message_ID = (((received_message.__str__()).split('ID: ', 1)[1])[0:8])
+
                 if (received_message_ID == '18ff50e5'):
+                    # voltage reading and conversion
                     charge_voltage_integer = (received_message.data[0]*256) + (received_message.data[1])
                     charge_voltage_float_whole = int(str(charge_voltage_integer)[0:2])
                     charge_voltage_float_fractional = int(str(charge_voltage_integer)[2])
                     charge_voltage_float = charge_voltage_float_whole + charge_voltage_float_fractional/10
-                    # charge_voltage_float
+                    
+                    # voltage publsihing ROS
                     msg_voltage = Float32()
                     msg_voltage.data = charge_voltage_float
                     self.charger_publisher_voltage.publish(msg_voltage)
+
+                    # current reading and conversion
+                    charge_current_integer = (received_message.data[2]*256) + (received_message.data[3])
+                    if (charge_current_integer < 10):
+                        charge_current_float_whole = int(('00'+str(charge_current_integer))[0:2])
+                        charge_current_float_fractional = int(('00'+str(charge_current_integer))[2])
+                    elif (charge_current_integer < 100):
+                        charge_current_float_whole = int(('0'+str(charge_current_integer))[0:2])
+                        charge_current_float_fractional = int(('0'+str(charge_current_integer))[2])
+                    else:
+                        charge_current_float_whole = int(str(charge_current_integer)[0:2])
+                        charge_current_float_fractional = int(str(charge_current_integer)[2])
+                    charge_current_float = charge_current_float_whole + charge_current_float_fractional/10
+                    
+                    # current publsihing ROS
+                    msg_current = Float32()
+                    msg_current.data = charge_current_float
+                    self.charger_publisher_current.publish(msg_current)
+                    
+                    # stop looping, as a relevant CAN message was received
                     break
+
                 else:
+                    # pass, so the while loop can continue searching for a relevant CAN message
                     pass
         except:
             pass
